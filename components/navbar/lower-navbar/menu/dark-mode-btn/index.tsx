339b97e5caf9289ae019408003fdd62a8dc39237
Diwakar Jha <diwakarjha554@gmail.com>
2024-08-03 20:32:42 +0530

    optimize menu icons

--- components/navbar/lower-navbar/menu/dark-mode-btn/index.tsx
+++ components/navbar/lower-navbar/menu/dark-mode-btn/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ThemeSwitcher from '@/components/ui/theme/themeSwitcher';
 import { MdDarkMode } from 'react-icons/md';
+import { CgDarkMode } from 'react-icons/cg';
 
 const DarkModeBtn = () => {
     return (
         <div className='px-3 h-16 bg-background flex items-center justify-between'>
             <div className='flex items-center gap-3'>
                 <div className='bg-gray-300/70 dark:bg-gray-500/50 p-2 rounded-full'>
-                    <MdDarkMode size={20} />
+                    <CgDarkMode size={20} />
                 </div>
                 <h1 className='font-medium'>
                     Dark Mode
--- components/navbar/lower-navbar/menu/index.tsx
+++ components/navbar/lower-navbar/menu/index.tsx
@@ -6,6 +6,8 @@ import MenuItems from '@/components/navbar/lower-navbar/menu/menu-items';
 import DarkModeBtn from '@/components/navbar/lower-navbar/menu/dark-mode-btn';
 import { HiOutlineMenuAlt3 } from 'react-icons/hi';
 import { FaFaceAngry } from 'react-icons/fa6';
+import { AiOutlineUser } from 'react-icons/ai';
+import { RiHeartFill, RiLogoutCircleRLine } from 'react-icons/ri';
 
 const Menu = () => {
 
@@ -23,11 +25,11 @@ const Menu = () => {
       </div>
       {isOpen && (
         <div className='absolute bg-background border top-12 right-0 rounded-xl w-64 p-1'>
-          <MenuItems href='' title='Account' icon={FaFaceAngry} className='bg-background rounded-xl hover:bg-gray-200 dark:hover:bg-gray-500/40' />
-          <MenuItems href='' title='Liked packages' icon={FaFaceAngry} className='bg-background rounded-xl hover:bg-gray-200 dark:hover:bg-gray-500/40' />
+          <MenuItems href='' title='Account' icon={AiOutlineUser} className='bg-background rounded-xl hover:bg-gray-200 dark:hover:bg-gray-500/40' />
+          <MenuItems href='' title='Wishlist' icon={RiHeartFill} className='bg-background rounded-xl hover:bg-gray-200 dark:hover:bg-gray-500/40' />
+          {/* <MenuItems href='' title='Account' icon={FaFaceAngry} className='bg-background rounded-xl hover:bg-gray-200 dark:hover:bg-gray-500/40' /> */}
           <DarkModeBtn />
-          <MenuItems href='' title='Account' icon={FaFaceAngry} className='bg-background rounded-xl hover:bg-gray-200 dark:hover:bg-gray-500/40' />
-          <MenuItems href='' title='Logout' icon={FaFaceAngry} className='bg-background rounded-xl hover:bg-gray-200 dark:hover:bg-gray-500/40' />
+          <MenuItems href='' title='Logout' icon={RiLogoutCircleRLine} className='bg-background rounded-xl hover:bg-gray-200 dark:hover:bg-gray-500/40' />
         </div>
       )}
     </div>
