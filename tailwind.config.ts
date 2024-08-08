546cc6bfdf0aa6cc4077d5b600d6a17de322dc44
Diwakar Jha <diwakarjha554@gmail.com>
2024-08-08 07:38:04 +0530

    logo optimize

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -21,11 +21,6 @@ const config = {
       screens: {
         'xxs': '290px',
         'xs': '360px',
-        'sm': '640px',
-        'md': '768px',
-        'lg': '1024px',
-        'xl': '1280px',
-        '2xl': '1536px',
       },
       colors: {
         border: "hsl(var(--border))",
