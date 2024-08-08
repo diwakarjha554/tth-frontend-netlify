5d7a6f5ea4e0940e594c5790f987edee86749c5f
Diwakar Jha <diwakarjha554@gmail.com>
2024-08-08 08:12:30 +0530

    temp logo edit

--- components/navbar/lower-navbar/index.tsx
+++ components/navbar/lower-navbar/index.tsx
@@ -4,12 +4,13 @@ import Container from '@/components/ui/features/Container';
 import Logo from '@/components/ui/features/Logo';
 import Menu from '@/components/navbar/lower-navbar/menu';
 import Navlinks from '@/components/navbar/lower-navbar/navlinks';
+import LogoFull from '@/components/ui/features/LogoFull';
 
 const LowerNavbar = () => {
     return (
         <Section className="py-2 shadow dark:shadow-gray-500/60 bg-background">
             <Container className="w-full flex justify-center items-center md:justify-between">
-                <Logo className="" />
+                <LogoFull />
                 <div className="hidden md:flex items-center gap-5">
                     <Navlinks />
                     <Menu />
--- components/ui/features/LogoFull.tsx
+++ components/ui/features/LogoFull.tsx
@@ -1,18 +1,29 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 const LogoFull = () => {
-  return (
-    <div className='relative h-8 w-80'>
-        <Image 
-            src='/logo3.png'
-            alt='Travel Trail Holidays Logo'
-            fill
-            quality={100}
-            className='object-contain'
-        />
-    </div>
-  )
-}
+    return (
+        <Link
+            href='/' 
+            className="relative h-9 w-72"
+        >
+            <Image
+                src="/logo-light.png"
+                alt="Travel Trail Holidays Logo"
+                fill
+                quality={100}
+                className="object-contain dark:hidden"
+            />
+            <Image
+                src="/logo-dark.png"
+                alt="Travel Trail Holidays Logo"
+                fill
+                quality={100}
+                className="object-contain hidden dark:block"
+            />
+        </Link>
+    );
+};
 
 export default LogoFull;
