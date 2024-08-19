b7ceb63794a24b4c8efe3dc1df8f6e216791e24b
Diwakar Jha <diwakarjha554@gmail.com>
2024-08-19 07:10:25 +0530

    adjusted cards

--- app/(Routes)/about-us/page.tsx
+++ app/(Routes)/about-us/page.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import AboutHeroSection from '@/components/about-us/hero-section';
 import React from 'react';
 
@@ -11,17 +10,3 @@ const page = () => {
 };
 
 export default page;
-=======
-import AboutHeroSection from '@/components/about-us/hero-section';
-import React from 'react';
-
-const page = () => {
-    return (
-        <main className="flex min-h-screen flex-col items-center justify-start min-w-screen overflow-x-hidden">
-            <AboutHeroSection />
-        </main>
-    );
-};
-
-export default page;
->>>>>>> 2aa6a9046d4f547693f75125295e3b64f240be2a
