71ca9d6c4411cba221b48097628773da83f3b5d6
Diwakar Jha <diwakarjha554@gmail.com>
2024-09-10 15:49:45 +0530

    add footer

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import SelectTheme from '@/components/home/select-theme';
 import TopValues from '@/components/home/top-values';
 import Navbar from '@/components/navbar';
 import WeekendTrips from '@/components/home/weekend-trips';
+import Footer from '@/components/footer';
 
 const page = () => {
     return (
@@ -18,6 +19,7 @@ const page = () => {
             <WeekendTrips />
             <TopValues />
             <DataCounter />
+            <Footer />
             <FooterBar />
         </main>
     );
--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import Section from '../ui/features/Section';
+import Container from '../ui/features/Container';
 
 const Footer = () => {
-    return <div>Footer</div>;
+    return (
+        <Section className="py-20 border-t-[1px]">
+            <Container>Footer</Container>
+        </Section>
+    );
 };
 
 export default Footer;
